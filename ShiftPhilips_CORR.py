a95c4eba99ed6bbecbd351d22c6547435c98a9ce
RomainViardLille <romain.viard@univ-lille.fr>
2025-01-18 00:00:02 +0100

    Daily commit 2025-01-18

--- ShiftPhilips_CORR.py
+++ ShiftPhilips_CORR.py
@@ -6,8 +6,13 @@ fileName=sys.argv[1]
 imgNifti = nb.load(fileName)
 data= imgNifti.get_fdata()
 
-for cpt in range(0,data.shape[3]):
-    data[:,:,:,cpt] = np.roll(data[:,:,:,cpt],-1,axis=0)
-    data[:,:,:,cpt] = np.roll(data[:,:,:,cpt],1,axis=1)
+#gestion d'un volume 4D vs un volume 3D
+if len(data.shape)>3 :
+	for cpt in range(0,data.shape[3]):
+		data[:,:,:,cpt] = np.roll(data[:,:,:,cpt],-1,axis=0)
+		data[:,:,:,cpt] = np.roll(data[:,:,:,cpt],1,axis=1)
+else :
+	data= np.roll(data,-1,axis=0)
+	data= np.roll(data,1,axis=1)
 
 nb.save(nb.Nifti1Image(data, imgNifti.affine ),fileName)
